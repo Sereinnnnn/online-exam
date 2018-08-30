7e3cf2e211ae7182437733f0f78ce336c84f3904
tangyi <1633736729@qq.com>
2018-08-30 21:09:58 +0800

    增加考试服务

---
+++ online-exam-modules/online-exam-exam/src/main/java/com/github/tangyi/exam/config/ResourceServerConfig.java
@@ -0,0 +1,26 @@
+package com.github.tangyi.exam.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * 资源服务器配置
+ *
+ * @author tangyi
+ * @date 2018-08-30-21:07
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
+}
