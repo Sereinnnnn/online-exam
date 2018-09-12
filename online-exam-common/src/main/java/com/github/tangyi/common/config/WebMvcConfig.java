07a6072be8663a0629d8c2f697d85a3e8620152a
tangyi <1633736729@qq.com>
2018-09-12 22:39:17 +0800

    增加token解析器

--- online-exam-common/src/main/java/com/github/tangyi/common/config/WebMvcConfig.java
+++ online-exam-common/src/main/java/com/github/tangyi/common/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.github.tangyi.common.config;
 
+import com.github.tangyi.common.web.TokenArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -8,14 +9,20 @@ import java.util.List;
 
 /**
  * mvc配置
+ *
  * @author tangyi
  * @date 2018-08-23-13:37
  */
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
+    /**
+     * 配置参数解析器，用于解析请求头的token
+     *
+     * @param argumentResolvers argumentResolvers
+     */
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-
+        argumentResolvers.add(new TokenArgumentResolver());
     }
 }
---
+++ online-exam-common/src/main/java/com/github/tangyi/common/web/TokenArgumentResolver.java
@@ -0,0 +1,74 @@
+package com.github.tangyi.common.web;
+
+import com.github.tangyi.common.constants.SecurityConstant;
+import com.github.tangyi.common.vo.Role;
+import com.github.tangyi.common.vo.UserVo;
+import com.xiaoleilu.hutool.util.StrUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 解析token
+ *
+ * @author tangyi
+ * @date 2018-09-12-22:05
+ */
+@Configuration
+public class TokenArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private static final Logger logger = LoggerFactory.getLogger(TokenArgumentResolver.class);
+
+    /**
+     * 传入的参数是UserVo类型时才做解析
+     *
+     * @param methodParameter methodParameter
+     * @return
+     */
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        return methodParameter.getParameterType().equals(UserVo.class);
+    }
+
+    /**
+     * 解析token
+     *
+     * @param methodParameter       methodParameter
+     * @param modelAndViewContainer modelAndViewContainer
+     * @param nativeWebRequest      nativeWebRequest
+     * @param webDataBinderFactory  webDataBinderFactory
+     * @return Object
+     * @throws Exception
+     */
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
+        HttpServletRequest request = nativeWebRequest.getNativeRequest(HttpServletRequest.class);
+        String username = request.getHeader(SecurityConstant.USER_HEADER),
+                roles = request.getHeader(SecurityConstant.ROLE_HEADER);
+        if (StrUtil.isBlank(username) || StrUtil.isBlank(roles)) {
+            logger.warn("username or role is empty");
+            return null;
+        }
+        logger.info("username :{} roles:{}", username, roles);
+        UserVo userVo = new UserVo();
+        userVo.setUsername(username);
+        List<Role> roleList = new ArrayList<>();
+        Arrays.stream(roles.split(",")).forEach(roleString -> {
+            Role role = new Role();
+            role.setRoleName(roleString);
+            roleList.add(role);
+        });
+        userVo.setRoleList(roleList);
+        return userVo;
+    }
+}
