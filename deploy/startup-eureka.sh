fc1450086ed68ce1260ad0c1c79aff733a19790f
tangyi <1633736729@qq.com>
2018-09-29 21:22:08 +0800

    增加部署脚本

---
+++ deploy/startup-eureka.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+
+## service name
+APP_NAME=online-exam-eureka
+
+SERVICE_DIR=/online-exam
+SERVICE_NAME=$APP_NAME
+JAR_NAME=$SERVICE_NAME\.jar
+PID=$SERVICE_NAME\.pid
+
+case "$1" in
+
+    start)
+        nohup java -Xms256m -Xmx512m -jar $JAR_NAME >/dev/null 2>&1 &
+        echo $! > $SERVICE_DIR/$PID
+        echo "=== start $SERVICE_NAME"
+        ;;
+
+    stop)
+        kill `cat $SERVICE_DIR/$PID`
+        rm -rf $SERVICE_DIR/$PID
+        echo "=== stop $SERVICE_NAME"
+
+        sleep 5
+        ;;
+
+    restart)
+        $0 stop
+        sleep 2
+        $0 start
+        echo "=== restart $SERVICE_NAME"
+        ;;
+
+    *)
+        ## restart
+        $0 stop
+        sleep 2
+        $0 start
+        ;;
+esac
+exit 0
+
+
