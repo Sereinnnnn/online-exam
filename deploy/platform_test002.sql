ea34f7262144f6d52bac30ad380a21a825ee6a7e
tangyi <1633736729@qq.com>
2018-11-10 18:02:36 +0800

    提交mapper

--- deploy/platform_test002.sql
+++ deploy/platform_test002.sql
@@ -1,87 +1,143 @@
 /*
- Navicat Premium Data Transfer
+Navicat MySQL Data Transfer
 
- Source Server         : 144
- Source Server Type    : MySQL
- Source Server Version : 50710
- Source Host           : 192.168.0.144:3306
- Source Schema         : platform_test002
+Source Server         : mysql_144
+Source Server Version : 50710
+Source Host           : 192.168.0.144:3306
+Source Database       : platform_test002
 
- Target Server Type    : MySQL
- Target Server Version : 50710
- File Encoding         : 65001
+Target Server Type    : MYSQL
+Target Server Version : 50710
+File Encoding         : 65001
 
- Date: 08/11/2018 23:01:14
+Date: 2018-11-09 13:41:10
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
 -- Table structure for answer
 -- ----------------------------
 DROP TABLE IF EXISTS `answer`;
-CREATE TABLE `answer`  (
-  `id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '主键',
-  `user_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户ID',
-  `examination_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '考试ID',
-  `course_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '课程ID',
-  `subject_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '题目ID',
-  `answer` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '答案',
-  `creator` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建人',
-  `create_date` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建时间',
-  `modifier` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '修改人',
-  `modify_date` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '修改时间',
-  `del_flag` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '删除标记',
-  `application_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '系统编号',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `answer` (
+  `id` varchar(64) NOT NULL COMMENT '主键',
+  `user_id` varchar(64) DEFAULT NULL COMMENT '用户ID',
+  `examination_id` varchar(64) DEFAULT NULL COMMENT '考试ID',
+  `course_id` varchar(64) DEFAULT NULL COMMENT '课程ID',
+  `subject_id` varchar(64) DEFAULT NULL COMMENT '题目ID',
+  `answer` varchar(255) DEFAULT NULL COMMENT '答案',
+  `creator` varchar(255) DEFAULT NULL COMMENT '创建人',
+  `create_date` varchar(255) DEFAULT NULL COMMENT '创建时间',
+  `modifier` varchar(255) DEFAULT NULL COMMENT '修改人',
+  `modify_date` varchar(255) DEFAULT NULL COMMENT '修改时间',
+  `del_flag` varchar(20) DEFAULT NULL COMMENT '删除标记',
+  `application_code` varchar(255) DEFAULT NULL COMMENT '系统编号',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of answer
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for course
 -- ----------------------------
 DROP TABLE IF EXISTS `course`;
-CREATE TABLE `course`  (
-  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `course_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '课程名称',
-  `college` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '学院',
-  `major` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '专业',
-  `teacher` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '老师',
-  `creator` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建人',
-  `create_date` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建时间',
-  `modifier` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '修改人',
-  `modify_date` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '修改时间',
-  `del_flag` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '删除标记',
-  `application_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '系统编号',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `course` (
+  `id` varchar(255) NOT NULL,
+  `course_name` varchar(255) DEFAULT NULL COMMENT '课程名称',
+  `college` varchar(255) DEFAULT NULL COMMENT '学院',
+  `major` varchar(255) DEFAULT NULL COMMENT '专业',
+  `teacher` varchar(255) DEFAULT NULL COMMENT '老师',
+  `creator` varchar(255) DEFAULT NULL COMMENT '创建人',
+  `create_date` varchar(255) DEFAULT NULL COMMENT '创建时间',
+  `modifier` varchar(255) DEFAULT NULL COMMENT '修改人',
+  `modify_date` varchar(255) DEFAULT NULL COMMENT '修改时间',
+  `del_flag` varchar(20) DEFAULT NULL COMMENT '删除标记',
+  `application_code` varchar(255) DEFAULT NULL COMMENT '系统编号',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of course
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for examination
 -- ----------------------------
 DROP TABLE IF EXISTS `examination`;
-CREATE TABLE `examination`  (
-  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `examination_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '考试名称',
-  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '考试类型',
-  `attention` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '考试注意事项',
-  `start_time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '考试开始时间',
-  `end_time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '考试结束时间',
-  `duration` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '考试持续时间',
-  `total_score` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '总分',
-  `status` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '考试状态',
-  `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '封面',
-  `college` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '学院',
-  `major` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '专业',
-  `course` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '课程',
-  `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
-  `creator` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建人',
-  `create_date` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建时间',
-  `modifier` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '修改人',
-  `modify_date` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '修改时间',
-  `del_flag` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '删除标记',
-  `application_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '系统编号',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `examination` (
+  `id` varchar(255) NOT NULL,
+  `examination_name` varchar(255) DEFAULT NULL COMMENT '考试名称',
+  `type` varchar(255) DEFAULT NULL COMMENT '考试类型',
+  `attention` varchar(255) DEFAULT NULL COMMENT '考试注意事项',
+  `start_time` varchar(255) DEFAULT NULL COMMENT '考试开始时间',
+  `end_time` varchar(255) DEFAULT NULL COMMENT '考试结束时间',
+  `duration` varchar(255) DEFAULT NULL COMMENT '考试持续时间',
+  `total_score` varchar(255) DEFAULT NULL COMMENT '总分',
+  `status` varchar(255) DEFAULT NULL COMMENT '考试状态',
+  `avatar` varchar(255) DEFAULT NULL COMMENT '封面',
+  `college` varchar(255) DEFAULT NULL COMMENT '学院',
+  `major` varchar(255) DEFAULT NULL COMMENT '专业',
+  `course` varchar(255) DEFAULT NULL COMMENT '课程',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  `creator` varchar(255) DEFAULT NULL COMMENT '创建人',
+  `create_date` varchar(255) DEFAULT NULL COMMENT '创建时间',
+  `modifier` varchar(255) DEFAULT NULL COMMENT '修改人',
+  `modify_date` varchar(255) DEFAULT NULL COMMENT '修改时间',
+  `del_flag` varchar(20) DEFAULT NULL COMMENT '删除标记',
+  `application_code` varchar(255) DEFAULT NULL COMMENT '系统编号',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-SET FOREIGN_KEY_CHECKS = 1;
+-- ----------------------------
+-- Records of examination
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for exam_recode
+-- ----------------------------
+DROP TABLE IF EXISTS `exam_recode`;
+CREATE TABLE `exam_recode` (
+  `id` varchar(255) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL COMMENT '用户id',
+  `examination_id` varchar(255) DEFAULT NULL COMMENT '考试id',
+  `course_id` varchar(255) DEFAULT NULL COMMENT '课程id',
+  `exam_time` varchar(255) DEFAULT NULL COMMENT '考试时间',
+  `score` varchar(255) DEFAULT NULL COMMENT '成绩',
+  `creator` varchar(255) DEFAULT NULL COMMENT '创建人',
+  `create_date` varchar(255) DEFAULT NULL COMMENT '创建时间',
+  `modifier` varchar(255) DEFAULT NULL COMMENT '修改人',
+  `modify_date` varchar(255) DEFAULT NULL COMMENT '修改时间',
+  `del_flag` varchar(255) DEFAULT NULL COMMENT '删除标记',
+  `application_code` varchar(255) DEFAULT NULL COMMENT '系统编号',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of exam_recode
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for incorrect_answer
+-- ----------------------------
+DROP TABLE IF EXISTS `incorrect_answer`;
+CREATE TABLE `incorrect_answer` (
+  `id` varchar(255) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL COMMENT '考生ID',
+  `examination_id` varchar(255) DEFAULT NULL COMMENT '考试ID',
+  `subject_id` varchar(255) DEFAULT NULL COMMENT '题目ID',
+  `incorrect_answer` varchar(255) DEFAULT NULL COMMENT '错误答案',
+  `creator` varchar(255) DEFAULT NULL COMMENT '创建人',
+  `create_date` varchar(255) DEFAULT NULL COMMENT '创建时间',
+  `modifier` varchar(255) DEFAULT NULL COMMENT '修改人',
+  `modify_date` varchar(255) DEFAULT NULL COMMENT '修改时间',
+  `del_flag` varchar(255) DEFAULT NULL COMMENT '删除标记',
+  `application_code` varchar(255) DEFAULT NULL COMMENT '系统编号',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of incorrect_answer
+-- ----------------------------
+SET FOREIGN_KEY_CHECKS=1;
