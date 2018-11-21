8379a2e1ef988acf09d0f5c1509120148be34d66
tangyi <1633736729@qq.com>
2018-11-21 23:44:26 +0800

    增加选项

--- online-exam-modules/online-exam-exam/src/main/java/com/github/tangyi/exam/module/Subject.java
+++ online-exam-modules/online-exam-exam/src/main/java/com/github/tangyi/exam/module/Subject.java
@@ -31,6 +31,36 @@ public class Subject extends BaseEntity<Subject> {
     private String content;
 
     /**
+     * 选项A
+     */
+    private String optionA;
+
+    /**
+     * 选项B
+     */
+    private String optionB;
+
+    /**
+     * 选项C
+     */
+    private String optionC;
+
+    /**
+     * 选项D
+     */
+    private String optionD;
+
+    /**
+     * 选项E
+     */
+    private String optionE;
+
+    /**
+     * 选项F
+     */
+    private String optionF;
+
+    /**
      * 参考答案
      */
     private String answer;
@@ -112,5 +142,53 @@ public class Subject extends BaseEntity<Subject> {
 
     public void setLevel(String level) {
         this.level = level;
+    }
+
+    public String getOptionA() {
+        return optionA;
+    }
+
+    public void setOptionA(String optionA) {
+        this.optionA = optionA;
+    }
+
+    public String getOptionB() {
+        return optionB;
+    }
+
+    public void setOptionB(String optionB) {
+        this.optionB = optionB;
+    }
+
+    public String getOptionC() {
+        return optionC;
+    }
+
+    public void setOptionC(String optionC) {
+        this.optionC = optionC;
+    }
+
+    public String getOptionD() {
+        return optionD;
+    }
+
+    public void setOptionD(String optionD) {
+        this.optionD = optionD;
+    }
+
+    public String getOptionE() {
+        return optionE;
+    }
+
+    public void setOptionE(String optionE) {
+        this.optionE = optionE;
+    }
+
+    public String getOptionF() {
+        return optionF;
+    }
+
+    public void setOptionF(String optionF) {
+        this.optionF = optionF;
     }
 }
