156fb26449f8c9dce10be513c805538314a090d3
tangyi <1633736729@qq.com>
2019-01-03 18:17:45 +0800

    集成swagger

--- online-exam-modules/online-exam-user/src/main/java/com/github/tangyi/user/config/ValidateCodeConfig.java
+++ online-exam-modules/online-exam-user/src/main/java/com/github/tangyi/user/config/ValidateCodeConfig.java
@@ -20,12 +20,17 @@ public class ValidateCodeConfig {
     public DefaultKaptcha producer() {
         Properties properties = new Properties();
         properties.put("kaptcha.border", "no");
+        // 验证码字体颜色
         properties.put("kaptcha.textproducer.font.color", "black");
+        // 验证码字体大小
+        properties.put("kaptcha.textproducer.font.size", "30");
+        // 验证码文本字符间距
         properties.put("kaptcha.textproducer.char.space", "6");
+        // 验证码宽度
         properties.put("kaptcha.image.width", "105");
+        // 验证码高度
         properties.put("kaptcha.image.height", "35");
         properties.put("kaptcha.textproducer.char.length", "4");
-        properties.put("kaptcha.textproducer.font.size", "30");
         Config config = new Config(properties);
         DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
         defaultKaptcha.setConfig(config);
