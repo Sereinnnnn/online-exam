2478f2f3fc603de5111b9ca74ba60536c4828cb4
tangyi <1633736729@qq.com>
2019-01-05 12:18:02 +0800

    优化

--- online-exam-common/src/main/java/com/github/tangyi/common/config/WebMvcConfig.java
+++ online-exam-common/src/main/java/com/github/tangyi/common/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.github.tangyi.common.config;
 import com.github.tangyi.common.resolver.TokenArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import java.util.List;
@@ -27,5 +28,23 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new TokenArgumentResolver());
+    }
+
+    /**
+     * 解决集成swagger访问不了UI问题
+     *
+     * @param registry registry
+     * @author tangyi
+     * @date 2019/01/04 22:02
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
     }
 }
